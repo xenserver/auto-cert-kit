ca259f1ef2763dffbfcaba3238dc9fa0a135e8a7
Rob Dobson <rob@rdobson.co.uk>
2013-06-07 14:41:57 +0100

    Moving XenAPI to its own module, and including as part of the package in order to not fail unittests.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='AutoCertKit',
+      version='0.8.1',
+      author='Citrix System Inc.',
+      url='http://github.com/xen-org/auto-cert-kit',
+      packages=['autocertkit', 'XenAPI'],
+     )
+
+
