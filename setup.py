779e1e323e77e4befe44c422aebe725e19435380
Jun Wook Kwak <bpwook@gmail.com>
2013-11-03 16:14:43 +0000

    Updated ACK source url.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(name='AutoCertKit',
       version='0.8.1',
       author='Citrix System Inc.',
-      url='http://github.com/xen-org/auto-cert-kit',
+      url='http://github.com/xenserver/auto-cert-kit',
       packages=['autocertkit', 'XenAPI', 'acktools', 'acktools.net'],
      )
 
