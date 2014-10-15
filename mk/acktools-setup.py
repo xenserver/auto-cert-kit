c00701a6534b9dc3faa6c39973a71a815772ac03
JunWook Kwak <bpwook@gmail.com>
2014-10-15 14:24:48 +0100

    Relase new ACK v0.9.0 including XS 6.5 support.

--- mk/acktools-setup.py
+++ mk/acktools-setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='AutoCertKit',
-      version='0.8.1',
+      version='0.9.0',
       author='Citrix System Inc.',
       url='http://github.com/xen-org/auto-cert-kit',
       packages=['acktools', 'acktools.net'],
