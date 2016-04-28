9abe92109ad9679694f6efdb0cf2127ffe4167c4
JunWook Kwak <bpwook@gmail.com>
2016-04-28 20:56:19 +0100

    Updated to version 1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='AutoCertKit',
-      version='0.9.1',
+      version='1.0.0',
       author='Citrix System Inc.',
       url='http://github.com/xenserver/auto-cert-kit',
       packages=['autocertkit', 'XenAPI', 'acktools', 'acktools.net'],
