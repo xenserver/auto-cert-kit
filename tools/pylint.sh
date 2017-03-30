d4937d751689bf183ff17d14285294bc21220f3b
Vivek Kumar Chaubey <vivekkumar.chaubey@citrix.com>
2017-03-30 12:37:01 +0100

    Remove obsolete files

--- tools/pylint.sh
+++ tools/pylint.sh
@@ -2,9 +2,7 @@
 if [ $# -eq 0 ]
 then
 echo "ACK Code Violations:"
-files=`find -not -path "./XenAPI/*" |
-    egrep -e '\.(py)$$' -e 'plugins/autocertkit' |
-    sort | uniq`
+files=`find ../ | egrep -e '\.(py)$$' -e 'plugins/autocertkit' | sort | uniq`
 else
 files="$1"
 fi 
